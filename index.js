010930855674575ce522aa7909e5055c443b7ddf
Juan Alvarado <jalvarado91@gmail.com>
2015-09-13 06:01:08 -0400

    foosball semiworking once side

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var Firebase = require("Firebase");
 var myFirebaseRef = new Firebase("https://burning-fire-6346.firebaseio.com/");
 var player = "player1";
 
+var epsilon = 2;
+
+var d1 = 50;
+var d2 = 50;
+var d3 = 50;
+
 board.on("ready", function() {
   var proximity1 = new five.Proximity({
     controller: "HCSR04",
@@ -25,9 +31,14 @@ board.on("ready", function() {
 
   proximity1.on("data", function() {
   	var cm = Math.floor(this.cm)
-  	var distance1Ref = myFirebaseRef.child(player).child("sensors").child("distance1")
-    console.log("Sensor1: "+ cm + "cm", this.in + "in");
-    distance1Ref.update({"value": cm});
+  	if(Math.abs(d1 - cm) > epsilon) {
+  		d1 = cm;
+	  	var distance1Ref = myFirebaseRef.child(player).child("sensors").child("distance1");
+
+	    console.log("Sensor1: "+ d1 + "cm", this.in + "in");
+	    distance1Ref.update({"value": d1});
+
+    }
   });
 
   proximity1.on("change", function() {
@@ -36,10 +47,14 @@ board.on("ready", function() {
 
   proximity2.on("data", function() {
   	var cm = Math.floor(this.cm)
-    console.log("Sensor2: "+ cm + "cm", this.in + "in");
-    var distance2Ref = myFirebaseRef.child(player).child("sensors").child("distance2")
-    console.log("Sensor1: "+ cm + "cm", this.in + "in");
-    distance2Ref.update({"value": cm});
+  	if(Math.abs(d3 - cm) > epsilon) {
+  		d3 = cm;
+	  	var distance2Ref = myFirebaseRef.child(player).child("sensors").child("distance2");
+
+	    console.log("Sensor2: "+ d3 + "cm", this.in + "in");
+	    distance2Ref.update({"value": d3});
+
+    }
   });
 
   proximity2.on("change", function() {
@@ -48,10 +63,14 @@ board.on("ready", function() {
 
   proximity3.on("data", function() {
   	var cm = Math.floor(this.cm)
-    console.log("Sensor3: "+ cm + "cm", this.in + "in");
-    var distance3Ref = myFirebaseRef.child(player).child("sensors").child("distance3")
-    console.log("Sensor1: "+ cm + "cm", this.in + "in");
-    distance3Ref.update({"value": cm});
+  	if(Math.abs(d3 - cm) > epsilon) {
+  		d3 = cm;
+	  	var distance3Ref = myFirebaseRef.child(player).child("sensors").child("distance3");
+
+	    console.log("Sensor3: "+ d3 + "cm", this.in + "in");
+	    distance3Ref.update({"value": d3});
+
+    }
   });
 
   proximity3.on("change", function() {
